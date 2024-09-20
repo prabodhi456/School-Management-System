e31de5cb64593e6ea7d23e0ee3c153a01b40b368
prabodhi456 <prabodi7478@gmail.com>
2024-09-20 15:55:14 +0530

    some changes

--- data/views.py
+++ data/views.py
@@ -5,6 +5,6 @@ from .models import Student, Teacher
 def view_data(request):
     students = Student.objects.all()
     teachers = Teacher.objects.all()
-    return render(request, 'data_visualization/data_view.html', {'students': students, 'teachers': teachers})
+    return render(request, 'data/data_view.html', {'students': students, 'teachers': teachers})
 
 # Create your views here.
